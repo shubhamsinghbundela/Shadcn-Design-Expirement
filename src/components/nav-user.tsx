3bf2737a3e8f9fb42a3ffeb9b9ee828e1c6dd369
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-07 14:13:08 +0530

    change checkbalance to profle

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -79,7 +79,7 @@ export function NavUser({
             <DropdownMenuGroup>
               <DropdownMenuItem>
                 <IconUserCircle />
-                Check Balance
+                Profile
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
