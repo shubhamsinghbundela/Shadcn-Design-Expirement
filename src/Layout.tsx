e5ae7602aa37c560c84788ce7d89c7782d9920ba
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-07 14:13:59 +0530

    remove unwanted code

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,30 +1,14 @@
-import {
-  Sidebar,
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "16rem",
-          "--sidebar-width-mobile": "18rem",
-        } as React.CSSProperties
-      }
-    >
+    <SidebarProvider>
       <AppSidebar />
       <main>
         <SidebarTrigger />
         {children}
       </main>
-      {/* <Sidebar variant="sidebar" collapsible="icon" />
-      <SidebarInset>
-        <main>{children}</main>
-      </SidebarInset> */}
     </SidebarProvider>
   )
 }
